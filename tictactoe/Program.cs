ca2a37c8491f267d5af51ca5937beedc4e7a6b7d
Eduardo Olguin <eddo555@hotmail.com>
2021-03-30 12:37:00 +0200

    #region divided

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -12,15 +12,14 @@ namespace tictactoe
         static void Main(string[] args)
         {
             bool done = false;
-            
-            while (!done)
-            {
-                
+
+           
+
                 int player = 0;
+#region InitializeBoard
+            //initialize and display board
 
-                //initialize and display board
-
-                string[,] board = new string[3, 3]
+            string[,] board = new string[3, 3]
                 {
                 { "1", "2", "3" },
                 { "4", "5", "6" },
@@ -39,7 +38,12 @@ namespace tictactoe
                     }
                     Console.WriteLine();
                 }
+#endregion InitializeBoard
 
+            do
+            {
+#region userInput
+                
 
                 for (int k = 0; k < 10; k++)
                 {
@@ -267,27 +271,68 @@ namespace tictactoe
                         }
 
                     }
-                                                            
+
+#region SetUpdatedBoard
                     for (int i = 0; i < rowLength; i++)
                     {
+                        Console.WriteLine(done);
                         for (int j = 0; j < colLength; j++)
                         {
                             Console.Write(" {0} ", board[i, j] + "\t");
 
                         }
                         Console.WriteLine();
-                        
+
                     }
+#endregion SetUpdatedBoard
+#region CheckForWinner
+                    //check for winner
                     if (board[0, 0] == board[0, 1] && board[0, 0] == board[0, 2])
                     {
-                        
+
                         Console.WriteLine("endgame");
+                        
                         done = true;
+                        Console.WriteLine(done);
                     }
+                    if (board[1, 0] == board[1, 1] && board[1, 0] == board[1, 2])
+                    {
+
+                        Console.WriteLine("endgame");
+
+                        done = true;
+                        Console.WriteLine(done);
+                    }
+                    if (board[2, 0] == board[2, 1] && board[2, 0] == board[2, 2])
+                    {
+
+                        Console.WriteLine("endgame");
+
+                        done = true;
+                        Console.WriteLine(done);
+                    }
+                    if (board[0, 0] == board[1, 1] && board[0, 0] == board[2, 2])
+                    {
+
+                        Console.WriteLine("endgame");
+
+                        done = true;
+                        Console.WriteLine(done);
+                    }
+                    if (board[0, 2] == board[1, 1] && board[0, 2] == board[2, 0])
+                    {
+
+                        Console.WriteLine("endgame");
+
+                        done = true;
+                        Console.WriteLine(done);
+                    }
+#endregion CheckForWinner
 
                 }
-                Console.Read();
-            }            
+                #endregion userInput
+            } while (done == false);
+                Console.Read();                      
         }                     
     }
 }
