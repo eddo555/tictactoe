8f964350d8f51103c9bba4b727ab56d13f0f1318
Eduardo Olguin <eddo555@hotmail.com>
2021-03-30 11:39:12 +0200

    no end function

--- tictactoe/Program.cs
+++ tictactoe/Program.cs
@@ -3,123 +3,271 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace tictactoe
 {
     class Program
-    {
+    {     
         static void Main(string[] args)
         {
-            bool start = true;
-            while (start == true)
-            {
-
+            bool done = false;
             
-            int player = 0;
-            
-            //initialize and display board
-
-            string[,] board = new string[3, 3] 
-            { 
-                { "1", "2", "3" }, 
-                { "4", "5", "6" }, 
-                { "7", "8", "9" } 
-            };
-
-            int rowLength = board.GetLength(0);
-            int colLength = board.GetLength(1);
-
-            for (int i = 0; i < rowLength; i++)
+            while (!done)
             {
-                for (int j = 0; j < colLength; j++)
+                
+                int player = 0;
+
+                //initialize and display board
+
+                string[,] board = new string[3, 3]
                 {
-                    Console.Write(" {0} ", board[i, j] + "\t");
+                { "1", "2", "3" },
+                { "4", "5", "6" },
+                { "7", "8", "9" }
+                };
 
-                }
-                Console.WriteLine();
-            }
+                int rowLength = board.GetLength(0);
+                int colLength = board.GetLength(1);
 
-            for (int k = 0; k < 10; k++)
-            {
-                if (player % 2 == 0)
+                for (int i = 0; i < rowLength; i++)
                 {
-                    Console.WriteLine("Player 1: enter value to cross");
-                    string value = Console.ReadLine();
-
-                    switch (value)
+                    for (int j = 0; j < colLength; j++)
                     {
-                        case "1":
-                            board[0, 0] = "X";
-                            break;
-                        case "2":
-                            board[0, 1] = "X";
-                            break;
-                        case "3":
-                            board[0, 2] = "X";
-                            break;
-                        case "4":
-                            board[1, 0] = "X";
-                            break;
-                        case "5":
-                            board[1, 1] = "X";
-                            break;
-                        case "6":
-                            board[1, 2] = "X";
-                            break;
-                        case "7":
-                            board[2, 0] = "X";
-                            break;
-                        case "8":
-                            board[2, 1] = "X";
-                            break;
-                        case "9":
-                            board[2, 2] = "X";
-                            break;
-                        default:
-                            Console.WriteLine("invalid selection");
-                            break;
+                        Console.Write(" {0} ", board[i, j] + "\t");
+
                     }
+                    Console.WriteLine();
                 }
-                else
+
+
+                for (int k = 0; k < 10; k++)
                 {
-
-                    Console.WriteLine("Player 2: enter value to cross");
-                    string value = Console.ReadLine();
-                    switch (value)
+                    if (player % 2 == 0)
                     {
-                        case "1":
-                            board[0, 0] = "O";
-                            break;
-                        case "2":
-                            board[0, 1] = "O";
-                            break;
-                        case "3":
-                            board[0, 2] = "O";
-                            break;
-                        case "4":
-                            board[1, 0] = "O";
-                            break;
-                        case "5":
-                            board[1, 1] = "O";
-                            break;
-                        case "6":
-                            board[1, 2] = "O";
-                            break;
-                        case "7":
-                            board[2, 0] = "O";
-                            break;
-                        case "8":
-                            board[2, 1] = "O";
-                            break;
-                        case "9":
-                            board[2, 2] = "O";
-                            break;
-                        default:
-                            Console.WriteLine("invalid selection");
-                            break;
-                    }
+                        Console.WriteLine("Player 1: enter value to cross");
+                        string value = Console.ReadLine();
 
-                }
+                        switch (value)
+                        {
+                            case "1":
+
+                                if (board[0, 0].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[0, 0] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "2":
+                                if (board[0, 1].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[0, 1] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "3":
+                                if (board[0, 2].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[0, 2] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "4":
+                                if (board[1, 0].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[1, 0] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "5":
+                                if (board[1, 1].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[1, 1] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "6":
+                                if (board[1, 2].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[1, 2] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "7":
+                                if (board[2, 0].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[2, 0] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "8":
+                                if (board[2, 1].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[2, 1] = "X";
+                                    player++;
+                                }
+                                break;
+                            case "9":
+                                if (board[2, 2].Equals("O"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[2, 2] = "X";
+                                    player++;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("invalid selection");
+                                break;
+                        }
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("Player 2: enter value to cross");
+                        string value = Console.ReadLine();
+                        switch (value)
+                        {
+                            case "1":
+                                if (board[0, 0].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[0, 0] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "2":
+                                if (board[0, 1].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[0, 1] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "3":
+                                if (board[0, 2].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[0, 2] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "4":
+                                if (board[1, 0].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[1, 0] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "5":
+                                if (board[1, 1].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[1, 1] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "6":
+                                if (board[1, 2].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[1, 2] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "7":
+                                if (board[2, 0].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[2, 0] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "8":
+                                if (board[2, 1].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[2, 1] = "O";
+                                    player++;
+                                }
+                                break;
+                            case "9":
+                                if (board[2, 2].Equals("X"))
+                                {
+                                    Console.WriteLine("number already taken");
+                                }
+                                else
+                                {
+                                    board[2, 2] = "O";
+                                    player++;
+                                }
+                                break;
+                            default:
+                                Console.WriteLine("invalid selection");
+                                break;
+                        }
+
+                    }
+                                                            
                     for (int i = 0; i < rowLength; i++)
                     {
                         for (int j = 0; j < colLength; j++)
@@ -128,23 +276,18 @@ namespace tictactoe
 
                         }
                         Console.WriteLine();
+                        
                     }
-                    
+                    if (board[0, 0] == board[0, 1] && board[0, 0] == board[0, 2])
+                    {
+                        
+                        Console.WriteLine("endgame");
+                        done = true;
+                    }
 
-                    player++;
-            }
-           
-                
-            
-                
-             
-
-
-            Console.Read();
-            }
-
-        }
-
-        
+                }
+                Console.Read();
+            }            
+        }                     
     }
 }
